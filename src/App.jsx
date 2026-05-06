f9fdf26329e4f457dd61974d40ecd27d01805f5e
Yuki Yamashita <yamashita-y@example.com>
2026-05-06 18:15:47 +0900

    import修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { quizData } from "./questions.js";
+const QUESTIONS = quizData;
 import { useState, useEffect, useRef } from "react";
 import { quizData } from './questions.js';
 const QUESTIONS = quizData || [];
